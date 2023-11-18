6bf28237066472e8f70c0fb0cb8ad21a8a611d6e
mahmoud <mahmoud123@gmail.com>
2023-11-18 18:20:19 +0200

    edit footer

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,25 +1,13 @@
-<footer class="bg-light  pt-3 mb-2 ">
+<footer class="bg-light pt-3 mb-2 ">
     <div class="text-center text-md-start mt-5 container">
-        <div class="row mt-3">
-            <div class="col-md-5 mb-4">
+        <div class="row ">
+            <div class="col-md-6">
                 <h4 class="text-end fw-bold">من نحن</h4>
                 <article class="text-end lh-lg fw-bold text-muted w-75">
                     نحن نفتخر بأننا منصة تعليمية عبر الإنترنت تسعى جاهدة لتمكين الأفراد وتمكينهم من تطوير مهاراتهم وتحقيق أهدافهم. نحن نؤمن بأهمية التعليم كوسيلة لتحسين الحياة وزيادة الفرص. نحن هنا لنجعل هذه الفرص متاحة للجميع، بغض النظر عن خلفيتهم أو موقعهم.
                 </article>
             </div>
-            <div class="col-md-4 mb-4">
-                <h4 class="text-end fw-bold">الكورسات الحاليه</h4>
-                <!-- <ul class="text-end text-muted">
-                    <li><b>Html</b></li>
-                    <li><b>CSS-Bootstrap</b></li>
-                    <li><b>JavaScript</b></li>
-                    <li><b>ReactJS</b></li>
-                    <li><b>Php</b></li>
-                    <li><b>Python</b></li>
-                    <li><b>How to work</b></li>
-                </ul> -->
-            </div>
-            <div class="col-md-3 mb-4">
+            <div class="col-md-6">
                 <h4 class="text-end fw-bold">وسائل الدفع المتاحه</h4>
                 <div class="payMethod text-end">
                     <div class="paymentImg my-2">
@@ -30,15 +18,15 @@
                     <img src="{{ asset('assets/frontend/images/orangeCash.AVIF') }}" alt="orangeCash" width="40">
                     <img src="{{ asset('assets/frontend/images/etisalat-cash.AVIF') }}" alt="etisalat-cash" width="50">
                 </div>
-                <h4 class="text-end fw-bold">تواصل معنا</h4>
+                <!-- <h4 class="text-end fw-bold">تواصل معنا</h4>
                 <div class="socialMedia text-end">
                     <div class="socialIcons mt-4 text-end">
-                        <!-- https://www.facebook.com/profile.php?id=100089926454391 -->
+                        https://www.facebook.com/profile.php?id=100089926454391
                         <a href="" target="_blank" class="ms-2" ><i class="fa-brands fa-facebook fs-3 text-black" ></i></a>
-                        <!-- <a href="https:/wa.me/+201220407108" class="ms-2" target="_blank"><i class="fa-brands fa-square-whatsapp fs-3" style="color:#25D366"></i></a>
-                        <a href="mailto:yossefadel713@gmail.com" target="_blank" class="ms-2"><i class="fa-solid fa-envelope fs-3 text-dark"></i></a> -->
+                        <a href="https:/wa.me/+201220407108" class="ms-2" target="_blank"><i class="fa-brands fa-square-whatsapp fs-3" style="color:#25D366"></i></a>
+                        <a href="mailto:yossefadel713@gmail.com" target="_blank" class="ms-2"><i class="fa-solid fa-envelope fs-3 text-dark"></i></a>
                     </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </footer>
